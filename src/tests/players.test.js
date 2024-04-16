e8d26b5a1a036fee5135a603db7f9f0e612c1767
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-04-16 17:56:39 +0200

    Extra comments added

--- src/tests/players.test.js
+++ src/tests/players.test.js
@@ -2,6 +2,14 @@ import { newPlayer } from "../modules/players";
 
 // test that the new object contains everything
 test('Player contains player name', () => {
-    let testPlayer = newPlayer('Guy');
+    let testPlayer = newPlayer('Guy', 'player');
     expect(testPlayer.name).toMatch('Guy');
-})
+    expect(testPlayer.carrier.length).toBe(5);
+    expect(testPlayer.destroyer.length).toBe(2);
+});
+
+// test that computer player has been created 
+test('Check player type for CPU', () => {
+    let testPlayer = newPlayer('Computer', 'CPU');
+    expect(testPlayer.type).toMatch('CPU')
+});
